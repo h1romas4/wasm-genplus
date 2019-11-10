9776e64ceba544e0aa2c282361749b7afe903bcc
h1romas4 <h1romas4@gmail.com>
2019-11-10 15:37:01 +0900

    remove comments

--- src/main/c/wasm/fileio.c
+++ src/main/c/wasm/fileio.c
@@ -11,15 +11,15 @@ int load_archive(char *filename, unsigned char *buffer, int maxsize, char *exten
     strncpy(extension, "BIN", 3);
     extension[3] = 0;
 
-    // 53 45 47 41
-    for(int i = 0; i < 4; i++) {
-        EM_ASM_({
-            console.log('rom_buffer: ' + $0.toString(16));
-        }, buffer[0x100 + i]);
-    }
-    EM_ASM_({
-        console.log('rom_size: ' + $0);
-    }, rom_size);
+    // // 53 45 47 41
+    // for(int i = 0; i < 4; i++) {
+    //     EM_ASM_({
+    //         console.log('rom_buffer: ' + $0.toString(16));
+    //     }, buffer[0x100 + i]);
+    // }
+    // EM_ASM_({
+    //     console.log('rom_size: ' + $0);
+    // }, rom_size);
 
     return rom_size;
 }
